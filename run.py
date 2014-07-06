2ddafbfd5cb90733e71a51ca76793a0fd654fc4b
Frankie <max00355@gmail.com>
2014-07-06 01:08:42 -0400

    Just set up the start to the giant project

---
+++ run.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from login import login
+from register import register
+from index import index
+from newthread import newthread
+
+app = Flask(__name__)
+app.debug = True
+app.register_blueprint(index)
+app.register_blueprint(login)
+app.register_blueprint(register)
+app.register_blueprint(newthread)
+
+
+if __name__ == "__main__":
+    app.run()
