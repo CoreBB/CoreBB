e43c58212d97189866eda54ae0f735dfb17e9ecd
Pillows <matthew.wong@outlook.com>
2014-07-07 23:40:39 -0400

    A couple of links added here and there

--- run.py
+++ run.py
@@ -3,9 +3,11 @@ from login import login
 from register import register
 from index import index
 #from newthread import newthread
+import database
 
 app = Flask(__name__)
 app.debug = True
+app.secret_key = database.getSeed()
 
 app.register_blueprint(index)
 app.register_blueprint(login)
